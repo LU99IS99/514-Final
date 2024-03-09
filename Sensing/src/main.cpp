60db7a071d7955db9d7412dcee0466b65a521eed
LU99IS99 <133922082+LU99IS99@users.noreply.github.com>
2024-03-09 15:57:41 -0800

    Update main.cpp

--- Sensing/src/main.cpp
+++ Sensing/src/main.cpp
@@ -8,7 +8,7 @@ BluetoothSerial SerialBT;
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  SerialBT.begin("UV Sensor"); // Start Bluetooth with device name "UV Sensor"
+  SerialBT.begin("UV Sensor"); 
   
   if (!uv.begin()) {
     Serial.println("Didn't find Si1133");
@@ -18,7 +18,7 @@ void setup() {
 }
 
 void loop() {
-  float UVindex = uv.readUV() / 100.0; // Convert to proper UV index value
-  SerialBT.println(UVindex); // Send UV index over Bluetooth
+  float UVindex = uv.readUV() / 100.0; 
+  SerialBT.println(UVindex); 
   delay(60000); // Send data every 60 seconds
 }
