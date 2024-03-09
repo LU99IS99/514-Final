6e3751d3758b4c0d13a409d06d5f1c65f8c49c03
LU99IS99 <133922082+LU99IS99@users.noreply.github.com>
2024-03-09 15:48:27 -0800

    Update main.cpp

--- Sensing/src/main.cpp
+++ Sensing/src/main.cpp
@@ -1,22 +1,24 @@
 #include <Wire.h>
 #include "Adafruit_SI1145.h"
+#include "BluetoothSerial.h"
 
 Adafruit_SI1145 uv = Adafruit_SI1145();
+BluetoothSerial SerialBT;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Wire.begin();
+  SerialBT.begin("UV Sensor"); // Start Bluetooth with device name "UV Sensor"
+  
   if (!uv.begin()) {
     Serial.println("Didn't find Si1133");
     while (1);
   }
-  Serial.println("Si1133 UV sensor ready!");
+  Serial.println("Si1133 is ready!");
 }
 
 void loop() {
-  float UVindex = uv.readUV();
-  // The UV index reading from the sensor needs to be divided by 100 to get the actual value
-  UVindex /= 100.0;  
-  Serial.print("UV Index: ");
-  Serial.println(UVindex);
-  delay(2000); // Delay between reads
+  float UVindex = uv.readUV() / 100.0; // Convert to proper UV index value
+  SerialBT.println(UVindex); // Send UV index over Bluetooth
+  delay(60000); // Send data every 60 seconds
 }
